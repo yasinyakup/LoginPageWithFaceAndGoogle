7889d4f51202e5a72e1b4ed62c65cfcfad81fb64
yasinyakup <Yasin@19841231>
2020-02-10 11:37:56 +0300

    Initial commit

---
+++ app/src/main/java/com/yaytec/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yaytec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
